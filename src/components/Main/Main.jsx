9895992d0ad2fe2f0f354bb687699cf4f36f93f2
Talha <atercan0658@gmail.com>
2022-11-01 17:52:17 +0100

    ending algorithm done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Die from "./Die";
 import { nanoid } from "nanoid";
 import "./Main.css";
 
 const Main = () => {
   const [dice, setDice] = useState(() => allNewDice());
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const allHeld = dice.every((die) => die.isHeld);
+    const firstValue = dice[0].value;
+    const isEqual = dice.every((die) => die.value === firstValue);
+    if (allHeld && isEqual) {
+      setTenzies(true);
+      console.log("You won!");
+    }
+  }, [dice]);
+
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
@@ -30,13 +42,24 @@ const Main = () => {
   });
 
   const rollDice = () => {
-    setDice(allNewDice());
+    setDice((oldDice) => {
+      return oldDice.map((die) => {
+        return die.isHeld
+          ? die
+          : { ...die, value: Math.ceil(Math.random() * 6) };
+      });
+    });
   };
 
   return (
     <main>
       <div className="tenzies-container">
         <div className="tenzies">
+          <h1 className="title">Tenzies</h1>
+          <p className="instructions">
+            Roll until all dice are the same. Click each die to freeze it at its
+            current value between rolls.
+          </p>
           <div className="dice-container">{dieComponents}</div>
           <button className="roll" onClick={rollDice}>
             Roll
