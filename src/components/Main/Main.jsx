5298117ea3c18ff5ac127c06aee044b005c170b6
Talha <atercan0658@gmail.com>
2022-11-01 20:54:31 +0100

    new game function and animation added

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Die from "./Die";
 import { nanoid } from "nanoid";
 import "./Main.css";
+import Confetti from "react-confetti";
 
 const Main = () => {
   const [dice, setDice] = useState(() => allNewDice());
@@ -13,7 +14,6 @@ const Main = () => {
     const isEqual = dice.every((die) => die.value === firstValue);
     if (allHeld && isEqual) {
       setTenzies(true);
-      console.log("You won!");
     }
   }, [dice]);
 
@@ -41,28 +41,41 @@ const Main = () => {
     return <Die key={die.id} value={die.value} die={die} holdDice={holdDice} />;
   });
 
+  const generateNewDice = () => {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  };
+
   const rollDice = () => {
     setDice((oldDice) => {
       return oldDice.map((die) => {
-        return die.isHeld
-          ? die
-          : { ...die, value: Math.ceil(Math.random() * 6) };
+        return die.isHeld ? die : generateNewDice();
       });
     });
+  };
+
+  const newGame = () => {
+    setTenzies(false);
+    return setDice(allNewDice());
   };
 
   return (
     <main>
       <div className="tenzies-container">
         <div className="tenzies">
+          {tenzies && <Confetti />}
           <h1 className="title">Tenzies</h1>
           <p className="instructions">
             Roll until all dice are the same. Click each die to freeze it at its
             current value between rolls.
           </p>
           <div className="dice-container">{dieComponents}</div>
-          <button className="roll" onClick={rollDice}>
-            Roll
+          {tenzies && <p className="win-message">You Won!</p>}
+          <button className="roll" onClick={tenzies ? newGame : rollDice}>
+            {tenzies ? "New Game" : "Roll"}
           </button>
         </div>
       </div>
